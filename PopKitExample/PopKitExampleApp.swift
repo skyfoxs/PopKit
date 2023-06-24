34f56d1a23ba3e80256d347f689af5ad1d0d8b7b
Pakornpat Sinjiranon <skyfox.ku@gmail.com>
2023-06-24 21:16:21 +0700

    Add PopKitExample target

---
+++ PopKitExample/PopKitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopKitExampleApp.swift
+//  PopKitExample
+//
+//  Created by Pakornpat Sinjiranon on 24/6/23.
+//
+
+import SwiftUI
+
+@main
+struct PopKitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
