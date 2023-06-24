b7586c680d7edeeaf329cb5863d3bfe8bac640e1
Pakornpat Sinjiranon <skyfox.ku@gmail.com>
2023-06-24 22:23:55 +0700

    Prepare PopKitGen for generating swift source code for PopKit

---
+++ PopKitGen/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  PopKitGen
+//
+//  Created by Pakornpat Sinjiranon on 24/6/23.
+//
+
+import PopKitGenHelper
+
+print("Background-Color".camelCased)
+
---
+++ PopKitGenHelper/PopKitGenHelper.h
@@ -0,0 +1,18 @@
+//
+//  PopKitGenHelper.h
+//  PopKitGenHelper
+//
+//  Created by Pakornpat Sinjiranon on 24/6/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PopKitGenHelper.
+FOUNDATION_EXPORT double PopKitGenHelperVersionNumber;
+
+//! Project version string for PopKitGenHelper.
+FOUNDATION_EXPORT const unsigned char PopKitGenHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PopKitGenHelper/PublicHeader.h>
+
+
